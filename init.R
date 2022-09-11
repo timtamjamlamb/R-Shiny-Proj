9352b4da45215deae5c3a6e371f442ee268da546
timtamjamlamb <49461184+timtamjamlamb@users.noreply.github.com>
2022-09-11 18:37:11 +1200

    Add files via upload

---
+++ init.R
@@ -0,0 +1,18 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("shiny", "stopwords",
+                "tokenizers", "SnowballC",
+                "tidyverse", "syn",
+                "wordcloud", "tm",
+                )
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
